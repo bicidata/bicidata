0642ac7efdb3575efc2664d406725ae588644ff4
Ismael Benito <ismael.benito.altamirano@gmail.com>
2021-09-12 16:44:09 +0200

    Parsing GBFS API correctly.

--- apps/api/functions/index.js
+++ apps/api/functions/index.js
@@ -4,9 +4,11 @@ const functions = require("firebase-functions");
 // A requester module to consume API
 const fetch = require('node-fetch');
 
-// // The Firebase Admin SDK to access Firestore.
-// const admin = require('firebase-admin');
-// admin.initializeApp();
+// The Firebase Admin SDK to access Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+let API_URL = "http://35.241.203.225";
 
 // A function to serve the modified GBFS API:
 exports.gbfs = functions
@@ -18,12 +20,19 @@ exports.gbfs = functions
         let response_gbfs = await fetch(url, settings);
         let json_gbfs = await response_gbfs.json();
 
-        // json_gbfs["data"]["en"]["feeds"].push({
-        //     name: "snapshots_information",
-        //     url: "http://35.241.203.225/snapshots.json"
-        // })
+        json_gbfs["ttl"] = -1;
 
-        functions.logger.info(json_gbfs, {structuredData: true});
+        json_gbfs["data"]["en"]["feeds"].push({
+            name: "station_status_snapshots",
+            url: `${API_URL}/station_status/{timestamp}.json`
+        })
+
+        json_gbfs["data"]["en"]["feeds"].push({
+            name: "snapshots_information",
+            url: `${API_URL}/snapshots.json`
+        })
+
+        // functions.logger.info(json_gbfs, {structuredData: true});
 
         response.json(json_gbfs);
       }
@@ -34,10 +43,28 @@ exports.gbfs = functions
 
 
 // A function to capture the GBFS snapshots:
-exports.scheduledFunction = functions
+exports.gbfsSnapshot = functions
     // .region('europe-west1')
     .pubsub.schedule('every minute')
-    .onRun((context) => {
-  functions.logger.info('This will be run every minute!', {structuredData: true});
-  return null;
-});
+    .onRun(async (context) => {
+         let settings = { method: "Get" };
+         let url = "https://barcelona-sp.publicbikesystem.net/customer/ube/gbfs/v1/en/station_status";
+         let response_gbfs = await fetch(url, settings);
+         let json_gbfs = await response_gbfs.json();
+
+         let timestamp = json_gbfs["last_updated"];
+         let date = new Date(timestamp * 1000);
+
+         json_gbfs["ttl"] = -1;
+
+         // Push the new snapshot into Firestore using the Firebase Admin SDK.
+         const writeResult = await admin.firestore()
+             .collection(`snapshots_${date.getFullYear()}-${date.getMonth() + 1}-${date.getDay()}`)
+             .doc(timestamp.toString())
+             .set(json_gbfs);
+
+         functions.logger.info("Success writing snapshoot.", {structuredData: true});
+
+         return null;
+        }
+    );
